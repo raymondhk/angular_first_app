cc018b4a86aa18b631d07ec56ddebf77b0999fa6
Raymond Kim <ray.kim09@gmail.com>
2017-10-10 13:57:54 -0400

    completed first app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms'; // <-- Import FormsModule
+import { HttpModule } from '@angular/http'; // <-- Import HttpModule
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	      declarations: [
+		            AppComponent
+	      ],
+	      imports: [
+		            BrowserModule,
+		            FormsModule, // <-- Include module in our AppModules
+		            HttpModule // <-- Include module in our AppModules
+	      ],
+	      providers: [],
+	      bootstrap: [AppComponent]
 })
 export class AppModule { }
